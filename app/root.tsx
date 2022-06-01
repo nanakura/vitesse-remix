d797f07b4bcd2bd2be32dd732d6f6e43169f1a73
nanakura <1749094641@qq.com>
2022-06-01 14:02:57 +0800

    chore: unocss

--- app/root.tsx
+++ app/root.tsx
@@ -8,23 +8,49 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node"; // or "@remix-run/cloudflare"
+
+import styles from "./styles/uno.css";
+import Footer from "./components/Footer";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
 
+
 export default function App() {
+  
   return (
     <html lang="en">
       <head>
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
+      <body className="font-sans dark:text-white dark:bg-hex-121212">
+        <main className="font-sans px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+          <Outlet />
+          <Footer/>
+        </main>
         <ScrollRestoration />
-        <Scripts />
+        <Scripts as={
+          `(function() {
+            const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+            const setting = localStorage.getItem('usehooks-ts-dark-mode')
+              ? localStorage.getItem('usehooks-ts-dark-mode') === 'true'
+                ? 'dark'
+                : 'light'
+              : 'auto'
+      
+            if (setting === 'dark' || (prefersDark && setting !== 'light'))
+              document.documentElement.classList.toggle('dark', true)
+          })()`
+        }/>
         <LiveReload />
       </body>
     </html>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,46 @@
+import { useNavigate } from "@remix-run/react";
+import { useRef } from "react";
+
 export default function Index() {
+  const name = useRef<HTMLInputElement>(null)
+  const navigate = useNavigate()
+  const go = () => {
+    if (name.current)
+      navigate(`/hi/${encodeURIComponent(name.current.value)}`)
+  }
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <div className="i-carbon-campsite text-4xl inline-block" />
+      <p>
+        <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
+        Vitesse Remix
+        </a>
+      </p>
+      <p>
+        <em className="text-sm op75">Opinionated Remix.Run Starter Template</em>
+      </p>
+
+      <div className="py-4" />
+
+      <input
+        ref={name}
+        id="input"
+        placeholder="What's your name?"
+        type="text"
+        className="px-4 py-2 w-250px text-center bg-transparent outline-none outline-active:none border border-rounded border-gray-200 border-dark:gray-700"
+        onKeyDown={({ key }) => key === 'Enter' && go()}
+      />
+
+      <div>
+        <button
+          className="m-3 text-sm btn"
+          disabled={!name}
+          onClick={() => go() }
+        >
+        Go
+        </button>
+      </div>
     </div>
   );
 }
